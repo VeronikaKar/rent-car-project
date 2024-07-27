1bd38e66fa575075390c7dc7b4bee63763a1277d
VeronikaKaraul <veronika.life@gmail.com>
2024-07-27 20:38:17 +0300

    catalog

--- src/components/CatalogItem/CatalogItem.jsx
+++ src/components/CatalogItem/CatalogItem.jsx
@@ -1,20 +1,10 @@
-import { useState } from "react";
+import React from "react";
 import ModalWindow from "../ModalWindow/ModalWindow";
 import Icon from "../Icons/Icon";
 import s from "./CatalogItem.module.scss";
 
-import { useDispatch, useSelector } from "react-redux";
-import { addFavorite, deleteFavorite } from "../../redux/catalog/slice";
-import { selectFavorites } from "../../redux/catalog/selectors";
-
-const CatalogItem = ({ car }) => {
-  const dispatch = useDispatch();
-  const favorites = useSelector(selectFavorites);
-
-  const [isOpen, setIsOpen] = useState(false);
-  const [isFavorite, setIsFavorite] = useState(
-    favorites.some((item) => item.id === car.id)
-  );
+const CatalogItem = ({ car, isFavorite, onToggleFavorite }) => {
+  const [isOpen, setIsOpen] = React.useState(false);
 
   const handleOpenModal = () => {
     setIsOpen(true);
@@ -24,90 +14,117 @@ const CatalogItem = ({ car }) => {
     setIsOpen(false);
   };
 
-  const toggleFavorites = () => {
-    if (isFavorite) {
-      dispatch(deleteFavorite(car.id));
-    } else {
-      dispatch(addFavorite(car));
-    }
-    setIsFavorite(!isFavorite);
-  };
+  // Check if car.accessories and car.functionalities are arrays
+  const isArray = (value) => Array.isArray(value);
 
   return (
-    <>
-      <li key={car.id} className={s.card__item}>
-        <div className={s.card__wrapper}>
-          <img className={s.image} src={car.img} alt="car" />
-          <Icon isActive={isFavorite} onClick={toggleFavorites} />
-        </div>
-        <div className={s.card__text}>
-          <div className={s.card__title_box}>
-            <div>
-              {car.make}, {car.year}
-            </div>
-            <div>{car.rentalPrice}</div>
+    <div className={s.card__item}>
+      <div className={s.card__wrapper}>
+        <img
+          className={s.image}
+          src={car?.img || "default-image-url.jpg"}
+          alt={`${car?.make || "Default Make"} ${
+            car?.model || "Default Model"
+          }`}
+        />
+        <Icon isActive={isFavorite} onClick={() => onToggleFavorite(car)} />
+      </div>
+      <div className={s.card__text}>
+        <div className={s.card__title_box}>
+          <div>
+            {car?.make || "Default Make"}, {car?.year || "Default Year"}
           </div>
-          <p className={s.card__address}>{car.address}</p>
-
-          <ul className={s.card__description}>
-            <li className={s.description__item}>{car.rentalCompany}</li>
-            <li className={s.description__item}>{car.type}</li>
-            <li className={s.description__item}>{car.model}</li>
-            <li className={s.description__item}>{car.id}</li>
-          </ul>
+          <div>{car?.rentalPrice || "Default Price"}</div>
         </div>
-        <button className={s.card__learn_more_button} onClick={handleOpenModal}>
-          Learn More
-        </button>
-      </li>
+        <p className={s.card__address}>{car?.address || "Default Address"}</p>
+        <ul className={s.card__description}>
+          <li className={s.description__item}>
+            {car?.rentalCompany || "Default Rental Company"}
+          </li>
+          <li className={s.description__item}>{car?.type || "Default Type"}</li>
+          <li className={s.description__item}>
+            {car?.model || "Default Model"}
+          </li>
+          <li className={s.description__item}>{car?.id || "Default Id"}</li>
+        </ul>
+      </div>
+      <button className={s.card__learn_more_button} onClick={handleOpenModal}>
+        Learn More
+      </button>
       {isOpen && (
         <ModalWindow onClose={handleCloseModal}>
           <div className={s.modal__wrapper}>
-            <img src={car.img} alt="car" className={css.modal__image} />
+            <img
+              src={car?.img || "default-image-url.jpg"}
+              alt={`${car?.make || "Default Make"} ${
+                car?.model || "Default Model"
+              }`}
+              className={s.modal__image}
+            />
           </div>
           <div className={s.modal__description}>
             <div>
               <h2>
-                {car.make}
-                <span className={s.card__span}>{car.model}</span>, {car.year}
+                {car?.make || "Default Make"}
+                <span className={s.card__span}>
+                  {car?.model || "Default Model"}
+                </span>
+                , {car?.year || "Default Year"}
               </h2>
             </div>
             <div>
-              <p className={s.card__address}>{car.address}</p>
+              <p className={s.card__address}>
+                {car?.address || "Default Address"}
+              </p>
               <ul className={s.card__description}>
-                <li className={s.description__item}>Id: {car.id}</li>
-                <li className={s.description__item}>Year: {car.year}</li>
-                <li className={s.description__item}>Type: {car.type}</li>
                 <li className={s.description__item}>
-                  Fuel Consumption: {car.fuelConsumption}
+                  Id: {car?.id || "Default Id"}
                 </li>
                 <li className={s.description__item}>
-                  Engine Size: {car.engineSize}
+                  Year: {car?.year || "Default Year"}
+                </li>
+                <li className={s.description__item}>
+                  Type: {car?.type || "Default Type"}
+                </li>
+                <li className={s.description__item}>
+                  Fuel Consumption:{" "}
+                  {car?.fuelConsumption || "Default Fuel Consumption"}
+                </li>
+                <li className={s.description__item}>
+                  Engine Size: {car?.engineSize || "Default Engine Size"}
                 </li>
               </ul>
-              <p>{car.description}</p>
+              <p>{car?.description || "Default Description"}</p>
             </div>
             <div>
               <p>Accessories and functionalities:</p>
               <ul className={s.card__description}>
-                {car.accessories.map((item) => (
-                  <li className={s.description__item} key={item.id}>
-                    {item}
-                  </li>
-                ))}
-                {car.functionalities.map((item) => (
-                  <li className={s.description__item} key={item.id}>
-                    {item}
-                  </li>
-                ))}
+                {isArray(car?.accessories) &&
+                  car.accessories.map((item, index) => (
+                    <li
+                      className={s.description__item}
+                      key={`accessory-${index}-${item}`}
+                    >
+                      {item}
+                    </li>
+                  ))}
+                {isArray(car?.functionalities) &&
+                  car.functionalities.map((item, index) => (
+                    <li
+                      className={s.description__item}
+                      key={`functionality-${index}-${item}`}
+                    >
+                      {item}
+                    </li>
+                  ))}
               </ul>
             </div>
             <div>
               <p>Rental Conditions:</p>
               <ul>
-                <li>{car.rentalConditions}</li>
-                <li>Mileage: {car.mileage / 1000}</li>
-                <li>Price: {car.rentalPrice}</li>
+                <li>{car?.rentalConditions || "Default Rental Conditions"}</li>
+                <li>Mileage: {car?.mileage / 1000 || "Default Mileage"}</li>
+                <li>Price: {car?.rentalPrice || "Default Price"}</li>
               </ul>
             </div>
             <a href="tel:+380730000000" className={s.link}>
@@ -116,7 +133,7 @@ const CatalogItem = ({ car }) => {
           </div>
         </ModalWindow>
       )}
-    </>
+    </div>
   );
 };
 
--- src/pages/FavoritesPage/FavoritesPage.jsx
+++ src/pages/FavoritesPage/FavoritesPage.jsx
@@ -1,24 +1,21 @@
+// Example: FavoritesPage.jsx
+
 import React from "react";
-import { useSelector } from "react-redux";
-import { selectFavorites } from "../../redux/catalog/selectors";
 import CatalogItem from "../../components/CatalogItem/CatalogItem";
-import s from "./FavoritesPage.module.scss";
 
-const FavoritesPage = () => {
-  const favorites = useSelector(selectFavorites);
-
+const FavoritesPage = ({ favorites = [], onToggleFavorite }) => {
   return (
-    <div className={s.container}>
-      <h1>Your Favorites</h1>
-      {favorites.length === 0 ? (
-        <p>No favorites added yet.</p>
-      ) : (
-        <ul className={s.list}>
-          {favorites.map((carId) => (
-            <CatalogItem key={carId} carId={carId} isFavorite={true} />
-          ))}
-        </ul>
-      )}
+    <div>
+      <ul>
+        {favorites.map((car) => (
+          <CatalogItem
+            key={car.id} // Ensure that car.id is unique
+            car={car}
+            isFavorite={true}
+            onToggleFavorite={onToggleFavorite}
+          />
+        ))}
+      </ul>
     </div>
   );
 };
